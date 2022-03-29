44d62a8afb15deca740b5c03b8526f51755f509c
SRSteinkamp <simon.steinkamp@googlemail.com>
2022-03-29 10:33:18 +0200

    default dictionaries changed for piloting

--- ergEx_Run.py
+++ ergEx_Run.py
@@ -58,8 +58,8 @@ if __name__ == '__main__':
 
     expInfo = {'participant': '0', # Participant ID
                'eta': 1.0, # Dynamic of the experiment
-               'test_mode': True, # Whether an agent automatically presses buttons
-               'fullScreen': False, # Whether to use a full screen
+               'test_mode': False, # Whether an agent automatically presses buttons
+               'fullScreen': True, # Whether to use a full screen
                'calibration': False, # Whether to run calibrations before.
                'startPassive': 1, # Which run of the passive phase to start (starts at 1), if larger than MAX_RUN_PASSIVE, skips passive phase.
                'startActive': 1} # Which run of the active phase to start from (starts at 1)
