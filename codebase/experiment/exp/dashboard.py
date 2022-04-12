dee7e57c3b69fc9b8ad983fe781976258840d281
SRSteinkamp <simon.steinkamp@googlemail.com>
2022-04-12 13:15:33 +0200

    removed unnecessary lists

--- codebase/experiment/exp/dashboard.py
+++ codebase/experiment/exp/dashboard.py
@@ -445,18 +445,15 @@ def plot_to_trajectory(dataframe, ax):
 def plot_nonparametric_indifference_eta(dataframe, ax):
     trials = dataframe.query('event_type == "WealthUpdate"')
 
-    roots = []
-    min_max = []
     for n, ii in enumerate(trials.index):
         tmp_trial = trials.loc[ii, :]
         x_updates = wealth_change(x=tmp_trial.wealth,
                                   gamma=[tmp_trial.gamma_left_up, tmp_trial.gamma_left_down,
                                         tmp_trial.gamma_right_up, tmp_trial.gamma_right_down],
                                         lambd=tmp_trial.eta)
+
         root_dyn, func_dyn = indiference_eta(x_updates[0], x_updates[1], x_updates[2], x_updates[3], tmp_trial.wealth)
-        roots.append(root_dyn)
         min_max_dyn = calculate_min_v_max(root_dyn, func_dyn, tmp_trial.selected_side == 'left')
-        min_max.append(min_max)
 
         ax.plot(root_dyn[0], n, marker=min_max_dyn['sign'], color = min_max_dyn['color'])
 
