40a98c8f236d6ea617badc34bc8f3cabbf7678e7
SRSteinkamp <simon.steinkamp@googlemail.com>
2022-05-10 14:36:28 +0200

    added shifted indifference etas to constants / inputs

--- codebase/constants.py
+++ codebase/constants.py
@@ -1,7 +1,8 @@
 X0 = 1000
 N_REPEATS_PASSIVE = 10 #not used atm
 N_TRIALS_ACTIVE = 100 #not used atm
-INDIFFERENCE_ETAS = [-0.25, 0.0, 0.25, 0.5, 0.75, 1.0, 1.25]
+INDIFFERENCE_ETAS = {0.0: [-1.0, -0.5, 0.0, 0.5, 1.0, 1.5],
+                     1.0: [-0.5, 0.0, 0.5, 1.0, 1.5, 2.0]}
 INDIFFERENCE_X_0 = [1000]
 INDIFFERENCE_DX2 = -250
 
