0007815eb96aeb0b66acc2ddb4c40a270ef5c05b
SRSteinkamp <simon.steinkamp@googlemail.com>
2022-05-11 13:26:29 +0200

    two negative fractals

--- codebase/constants.py
+++ codebase/constants.py
@@ -2,8 +2,9 @@ X0 = 1000
 N_REPEATS_PASSIVE = 10 #not used atm
 N_TRIALS_ACTIVE = 100 #not used atm
 INDIFFERENCE_ETAS = {0.0: [-0.5, 0.0, 0.5, 1.0, 1.5],
-                     1.0: [-0.5, 0.0, 0.5, 1.0, 1.5]}
-INDIFFERENCE_X_0 = [1000]
+                     1.0: [-0.5, 0.0, 0.5, 1.0, 1.5],
+                     0.5: [-0.5, 0.0, 0.5, 1.0, 1.5]}
+INDIFFERENCE_X_0 = [1000, 5000]
 INDIFFERENCE_DX2 = -250
 
 N_FRACTALS = 9
