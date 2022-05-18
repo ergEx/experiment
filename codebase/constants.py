3f4e48d621099bfe6c0db3884c1c391aaab1e62e
Benjamin Skjold <Benjamin.skjold123@hotmail.com>
2022-05-18 10:25:21 +0100

    Changed tested indifference etas

--- codebase/constants.py
+++ codebase/constants.py
@@ -1,9 +1,9 @@
 X0 = 1000
 N_REPEATS_PASSIVE = 10 #not used atm
 N_TRIALS_ACTIVE = 100 #not used atm
-INDIFFERENCE_ETAS = {0.0: [-0.5, 0.0, 0.5, 1.0, 1.5],
-                     1.0: [-0.5, 0.0, 0.5, 1.0, 1.5],
-                     0.5: [-0.5, 0.0, 0.5, 1.0, 1.5]}
+INDIFFERENCE_ETAS = {0.0: [-1, 0.5, 2],
+                     1.0: [-1, 0.5, 2],
+                     0.5: [-1, 0.25, 0.75]} #Has not been decided!
 INDIFFERENCE_X_0 = [1000, 5000]
 INDIFFERENCE_DX2 = -250
 
